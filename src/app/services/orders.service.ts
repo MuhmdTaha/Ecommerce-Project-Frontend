2d57f7f7adcfb8524c6df5561ed46dba4d85af56
Ahmed-behiery <ahmedbehiery96@gmail.com>
2021-05-27 02:56:07 +0200

    create orders services

--- src/app/services/orders.service.ts
+++ src/app/services/orders.service.ts
@@ -1,9 +1,41 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class OrdersService {
+  baseURL: string = 'http://localhost:4001/orders';
 
-  constructor() { }
+
+  constructor(private client: HttpClient) { }
+
+  getOrders() {
+    return this.client.get(`${this.baseURL}/`);
+  }
+
+  getOrder(id) {
+    return this.client.get(`${this.baseURL}/user/${id}`);
+  }
+  addOrder(userId) {
+    let payload = new HttpParams().set('user', userId);
+    return this.client.post(`${this.baseURL}`, payload);
+  }
+  deleteOrder(id) {
+    return this.client.delete(`${this.baseURL}/${id}`);
+  }
+  updateOrder(id, order) {
+    const payload = new HttpParams()
+      .set('user', order.user)
+      .set('date', order.date)
+      .set('price', order.price)
+      .set('status', order.status);
+    var i = 0;
+    order.products.forEach((prodObj) => {
+      payload.set(`products[${i}][product]`, prodObj.product);
+      payload.set(`products[${i}][quantity]`, prodObj.quantity);
+      i++;
+    });
+    return this.client.patch(`${this.baseURL}/${id}`, payload);
+  }
 }
