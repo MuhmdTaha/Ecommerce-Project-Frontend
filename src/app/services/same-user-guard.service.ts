85ebea42cac8eb83b6b78fcba18489479f3246b4
MuhmdGad <mohamedengineer944@gmail.com>
2021-05-27 03:33:36 +0200

    Adding same-user service

--- src/app/services/same-user-guard.service.ts
+++ src/app/services/same-user-guard.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import {
+  CanActivate,
+  Router,
+  RouterStateSnapshot,
+  ActivatedRouteSnapshot,
+} from '@angular/router';
+import { UsersService } from './users.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class SameUserGuardService {
-
-  constructor() { }
+export class SameUserGuardService implements CanActivate {
+  constructor(private usersService: UsersService, private router: Router) {}
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+    if (this.usersService.currentUser.id == route.paramMap.get('id'))
+      return true;
+    this.router.navigate(['/no-access']);
+    return false;
+  }
 }
