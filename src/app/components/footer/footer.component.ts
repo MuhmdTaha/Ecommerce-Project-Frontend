dae3edacd996a3aa69c2ed7721c3f5a73aab621e
abdullahnegm <abdullahahmed1997@hotmail.com>
2021-05-27 02:11:32 +0200

    footer

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NavbarService } from 'src/app/services/navbar.service';
 
 @Component({
   selector: 'app-footer',
@@ -7,9 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
-  constructor() { }
+  constructor(public navService:NavbarService) { }
 
   ngOnInit(): void {
+    // this.navService.show();
   }
 
 }
