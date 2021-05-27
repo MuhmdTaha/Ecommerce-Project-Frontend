f2927b3a0f6b42eb9472f8188ffebfc97245ece1
MuhmdGad <mohamedengineer944@gmail.com>
2021-05-27 03:32:55 +0200

    Adding navbar service

--- src/app/services/navbar.service.ts
+++ src/app/services/navbar.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class NavbarService {
+  visible: boolean;
 
-  constructor() { }
+  constructor() {
+    this.visible = true;
+  }
+  hide() {
+    this.visible = false;
+  }
+
+  show() {
+    this.visible = true;
+  }
+
+  toggle() {
+    this.visible = !this.visible;
+  }
 }
