c58b49088edabe86548bbf17d1c70ef8f9a380d2
aya-maher <ayamaherali9@gmail.com>
2021-05-27 02:18:49 +0200

    admin-auth-guard

--- src/app/services/admin-auth-guard.service.ts
+++ src/app/services/admin-auth-guard.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';  
+import { CanActivate } from '@angular/router';  
+import { UsersService } from './users.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AdminAuthGuardService {
+export class AdminAuthGuardService implements CanActivate {
 
-  constructor() { }
+  constructor(private router: Router, private usersService: UsersService) { }
+  canActivate() {
+    let user = this.usersService.currentUser;  
+    if(user && user.isAdmin)
+      return true;
+    this.router.navigate(['/no-access']);
+      return false;
+  }
+  CheckAdminOrNot() {
+    let user = this.usersService.currentUser;  
+    if(user && user.isAdmin)
+      return true;
+    
+    return false;
+  }
 }
--- src/app/services/auth-guard.service.ts
+++ src/app/services/auth-guard.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { CanActivate, Router, RouterStateSnapshot } from '@angular/router';
+import { UsersService } from './users.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class AuthGuardService {
-
-  constructor() { }
+export class AuthGuardService implements CanActivate {
+  constructor(private usersService: UsersService, private router: Router) {}
+  canActivate(route, state: RouterStateSnapshot) {
+    if (this.usersService.isLoggedIn()) return true;
+    this.router.navigate(['/login'], { queryParams: { returnUrl: state.url } });
+    return false;
+  }
 }
